7df993919091fb28bb43c2b882e5c8f21a9f8074
Marek Counts <mcounts@suse.com>
2019-06-11 09:54:54 -0400

    updates

--- src/client.rs
+++ src/client.rs
@@ -2,6 +2,8 @@
 
 use futures::Future;
 use hyper::client::connect::{Destination, HttpConnector};
+use rayon::prelude::*;
+use std::time::Instant;
 use tower_grpc::Request;
 use tower_hyper::{client, util};
 use tower_util::MakeService;
@@ -13,6 +15,25 @@ pub mod hello_world {
 pub fn main() {
     let _ = ::env_logger::init();
 
+    let mut list = Vec::with_capacity(10000);
+    for i in 0..10000 {
+        list.push(i);
+    }
+
+    let now = Instant::now();
+    let _:Vec<i32> = list.par_iter().map( |_| {
+        make_request_to_server();
+        return 1;
+    }).collect();
+    /*
+    for _ in 1..10000 {
+        make_request_to_server();
+    }
+    */
+    println!("Time Taken: {}", now.elapsed().as_millis());
+}
+
+pub fn make_request_to_server() {
     let uri: http::Uri = format!("http://[::1]:50051").parse().unwrap();
 
     let dst = Destination::try_from_uri(uri.clone()).unwrap();
@@ -40,13 +61,12 @@ pub fn main() {
                 name: "What is in a name?".to_string(),
             }))
         })
-        .and_then(|response| {
-            println!("RESPONSE = {:?}", response);
+        .and_then(|_| {
+            //println!("RESPONSE = {:?}", response);
             Ok(())
         })
         .map_err(|e| {
             println!("ERR = {:?}", e);
         });
-
     tokio::run(say_hello);
 }
