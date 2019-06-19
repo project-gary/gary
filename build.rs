c190e6076d0f190d70e3d25aeb10ff4620dfe533
Marek Counts <mcounts@suse.com>
2019-06-18 22:05:48 -0400

    fix build.rs

--- build.rs
+++ build.rs
@@ -1,13 +1,1 @@
-extern crate tower_grpc_build;
-
-use std::io::{stderr, Write};
-use std::process;
-
-fn main() {
-    // Build helloworld
-    tower_grpc_build::Config::new()
-        .enable_server(true)
-        .enable_client(true)
-        .build(&["proto/helloworld.proto"], &["proto/"])
-        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
-}
+fn main() {}
