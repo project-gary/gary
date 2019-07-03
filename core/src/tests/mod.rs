e30019a8f9e4f8ddadaeb3dc821798510e066409
Danny Sauer <dsauer@suse.com>
2019-07-03 13:20:59 -0500

    fix tests to align w/ new data structure

--- core/src/tests/mod.rs
+++ core/src/tests/mod.rs
@@ -12,46 +12,49 @@ pub mod tests {
         */
         let mut deployment = Deployment {
             version: "apps/v1".to_string(),
-            kind: "Deployment".to_string(),
+            kind: DeploymentType::Docker,
             metadata: Metadata {
                 name: Some("nginx-deployment".to_string()),
-                labels: HashMap::new(),
+                labels: Some(HashMap::new()),
             },
             spec: DeploymentSpec {
                 replicas: 3,
                 template: DeploymentTemplate {
                     metadata: Metadata {
                         name: None,
-                        labels: HashMap::new(),
+                        labels: Some(HashMap::new()),
                     },
-                    spec: Spec {
+                    spec: Spec::DockerSpec(DockerSpec {
                         containers: Vec::new(),
-                    },
+                    }),
                 },
             },
         };
 
+        //deployment
+        //    .metadata
+        //    .labels.unwrap()
+        //    .insert("app".to_string(), "nginx".to_string());
         deployment
             .metadata
             .labels
-            .insert("app".to_string(), "nginx".to_string());
+            .as_mut()
+            .map(|l| l.insert("app".to_string(), "nginx".to_string()));
         deployment
             .spec
             .template
             .metadata
             .labels
-            .insert("app".to_string(), "nginx".to_string());
-        let container = Container {
-            name: "nginx".to_string(),
-            image: "nginx:1.7.9".to_string(),
-            ports: vec![Ports { container_port: 80 }],
-        };
-        deployment
-            .spec
-            .template
-            .spec
-            .containers
-            .insert(0, container);
+            .as_mut()
+            .map(|l| l.insert("app".to_string(), "nginx".to_string()));
+        if let Spec::DockerSpec(ref mut spec) = deployment.spec.template.spec {
+            let container = Container {
+                name: "nginx".to_string(),
+                image: "nginx:1.7.9".to_string(),
+                ports: vec![Ports { container_port: 80 }],
+            };
+            spec.containers.insert(0, container);
+        } //TODO: else?
 
         let file_deploy: Deployment = serde_yaml::from_str(&contents).expect("not to fail");
         assert_eq!(deployment, file_deploy);
