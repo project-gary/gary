0afbcc9aaf1fc90aacbb53cdfef185278fde8e43
Marek Counts <mcounts@suse.com>
2019-08-01 23:29:57 -0400

    fmt :(

--- gary/build.rs
+++ gary/build.rs
@@ -1,6 +1,6 @@
-use std::process::Command;
 use std::env;
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -8,8 +8,11 @@ fn main() {
     println!("Here: {} ", out_dir);
     // note that there are a number of downsides to this approach, the comments
     // below detail how to improve the portability of these commands.
-    Command::new("cargo").args(&["--manifest-path ../../plugin-runtime-containerd", "--out-dir plugins/"])
-                       .status()
-                       .unwrap();
-
+    Command::new("cargo")
+        .args(&[
+            "--manifest-path ../../plugin-runtime-containerd",
+            "--out-dir plugins/",
+        ])
+        .status()
+        .unwrap();
 }
